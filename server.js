0fb02d7c9f3369d48f91d5109e6b9d8abdef5405
AdelRahuma <adel_bugbug@yahoo.com>
2023-11-25 02:13:59 +0000

    get data endpoint created

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const mysql = require("mysql2");
-const cors = require('cors');
-app.use(cors());
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
-
+app.use(cors());
 const pool = mysql.createPool({
   host: process.env.HOST,
   user: process.env.MYUSER,
@@ -48,7 +47,37 @@ app.post("/arduino-data", (req, res) => {
     });
   });
 });
+app.get("/arduino-data", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      console.error("Error connecting to database:", err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+    connection.changeUser({ database: process.env.DATABAS }, (err) => {
+      if (err) {
+        console.error("Error selecting database:", err);
+        connection.release();
+        res.status(500).send("Internal Server Error");
+        return;
+      }
 
+      const query = "SELECT * FROM TempSensor";
+      connection.query(query, (err, results) => {
+        connection.release();
+
+        if (err) {
+          console.error("Error executing query:", err);
+          res.status(500).send("Internal Server Error");
+          return;
+        }
+
+        //console.log(results);
+        res.status(200).send({ result: results });
+      });
+    });
+  });
+});
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
