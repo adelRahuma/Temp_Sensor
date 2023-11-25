16c9c6234e60a0b6f0be7cd026b8fd310cd4b511
AdelRahuma <adel_bugbug@yahoo.com>
2023-11-25 23:16:47 +0000

    server updated

--- server.js
+++ server.js
@@ -1,84 +1,45 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-const mysql = require("mysql2");
-const cors = require("cors");
+const mysql = require("mysql2/promise"); // Using promise-based MySQL
 
 const app = express();
-const port = 3000;
-app.use(cors());
+const port = process.env.PORT || 3000; // Use the specified port in the environment or default to 3000
+
 const pool = mysql.createPool({
   host: process.env.HOST,
   user: process.env.MYUSER,
   password: process.env.PASSWD,
   database: process.env.DATABASE,
+  waitForConnections: true, // Wait for available connection if the limit is reached
+  connectionLimit: 10, // Maximum number of connections in the pool
+  queueLimit: 0, // Unlimited queued connection requests
 });
 
 app.use(express.json());
-app.post("/arduino-data", (req, res) => {
-  const data = req.body.data;
-  //console.log(data,"data<<<<");
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.error("Error connecting to database:", err);
-      res.status(500).send("Internal Server Error");
-      return;
-    }
-    connection.changeUser({ database: process.env.DATABAS }, (err) => {
-      if (err) {
-        console.error("Error selecting database:", err);
-        connection.release();
-        res.status(500).send("Internal Server Error");
-        return;
-      }
 
-      const query = "INSERT INTO TempSensor(TEMP) values(?)";
-      connection.query(query, [data], (err, results) => {
-        connection.release();
+app.post("/arduino-data", async (req, res) => {
+  try {
+    const data = req.body.data;
 
-        if (err) {
-          console.error("Error executing query:", err);
-          res.status(500).send("Internal Server Error");
-          return;
-        }
+    // Use pool.getConnection() and connection.execute() for better handling of connections
+    const connection = await pool.getConnection();
+    await connection.changeUser({ database: process.env.DATABASE });
 
-        //console.log(results);
-        res.status(200).send("Data inserted successfully");
-      });
-    });
-  });
+    const query = "INSERT INTO TempSensor(TEMP) VALUES (?)";
+    const [results] = await connection.execute(query, [data]);
+
+    connection.release();
+
+    // Use status code 201 for resource creation
+    res.status(201).json({ message: "Data inserted successfully", results });
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
-app.get("/arduino-data", (req, res) => {
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.error("Error connecting to database:", err);
-      res.status(500).send("Internal Server Error");
-      return;
-    }
-    connection.changeUser({ database: process.env.DATABAS }, (err) => {
-      if (err) {
-        console.error("Error selecting database:", err);
-        connection.release();
-        res.status(500).send("Internal Server Error");
-        return;
-      }
 
-      const query = "SELECT * FROM TempSensor";
-      connection.query(query, (err, results) => {
-        connection.release();
-
-        if (err) {
-          console.error("Error executing query:", err);
-          res.status(500).send("Internal Server Error");
-          return;
-        }
-
-        //console.log(results);
-        res.status(200).send({ result: results });
-      });
-    });
-  });
-});
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+
 module.exports = app;
